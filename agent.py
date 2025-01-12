02857c1070198f70b18a93003fae52827eeff8c0
victorpdiniz <vpd@cin.ufpe.br>
2025-01-12 00:00:55 -0300

    feat: added collision identifier.

--- agent.py
+++ agent.py
@@ -1,5 +1,7 @@
 from utils.ssl.Navigation import Navigation
 from utils.ssl.base_agent import BaseAgent
+from utils.Geometry import Geometry
+from utils.Point import Point
 
 class ExampleAgent(BaseAgent):
     def __init__(self, id=0, yellow=False):
@@ -8,6 +10,14 @@ class ExampleAgent(BaseAgent):
     def decision(self):
         if len(self.targets) == 0:
             return
+
+        robot_point = Point(self.robot.x, self.robot.y)
+        dist_min_to_opponent = 0.2
+        
+        for i in range(1, 21):
+            opponent_point = Point(self.opponents[i].x, self.opponents[i].y)
+            if Geometry.dist_to(robot_point, opponent_point) < dist_min_to_opponent:
+                print(f"Robot collided with opponent {i}.")         
 
         target_velocity, target_angle_velocity = Navigation.goToPoint(self.robot, self.targets[0])
         self.set_vel(target_velocity)
